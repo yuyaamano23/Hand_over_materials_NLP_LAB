9f43b599da428fecc305b0f8f75bba6e4204c643
yuyaamano23 <nba.chako.nba@icloud.com>
2022-02-23 11:08:45 +0900

    modify: comments

--- roc計算用コード.py
+++ roc計算用コード.py
@@ -2,14 +2,15 @@ from sklearn import metrics
 import matplotlib.pyplot as plt
 import numpy as np
 
-test_y = [0,1,0]
+# test_y = [0,1,0,...]
+test_y = []
+# predict_y = [0.909771681 ,0.891080797 ,0.926733315,...]
 predict_y = []
 
 
 # FPR, TPR(, しきい値) を算出
 fpr, tpr, thresholds = metrics.roc_curve(test_y, predict_y)
 
-# ついでにAUCも
 auc = metrics.auc(fpr, tpr)
 
 # ROC曲線をプロット
